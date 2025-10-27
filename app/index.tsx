ee287c41613494b5529c0d75c31412a41c8ecc3a
ArafiFnya2 <raffiluthfiansyah@gmail.com>
2025-10-27 10:49:07 +0700

    buat sora

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,9 @@
 //import Latihan1 from "@/components/latihan/Latihan1";
-import LoginScreen from "@/components/tugas/LoginScreen";
+//import LoginScreen from "@/components/tugas/LoginScreen";
+//import Latihan2 from "@/components/latihan/Latihan2";
+//import Latihan3 from "@/components/latihan/Latihan3";
+//import Latihan4 from "@/components/latihan/Latihan4";
+import HomeScreen from "@/components/tugas/HomeScreen";
 import React, { useState } from "react";
 import { View } from "react-native";
 
@@ -7,9 +11,13 @@ export default function App() {
   const [nama, setNama] = useState("");
 
   return (
-    <View >
+    <View>
       {/*<Latihan1/>*/}
-      <LoginScreen />
+      {/*<LoginScreen />*/}
+      {/*<Latihan2/>*/}
+      {/*<Latihan3 />*/}
+      {/*<Latihan4/>*/}
+      <HomeScreen/>
     </View>
   );
 }
