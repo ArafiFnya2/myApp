4f561ce0f9b9c99f9088f07281d9e43ca521056b
ArafiFnya2 <raffiluthfiansyah@gmail.com>
2025-10-28 10:43:23 +0700

    buat farhan

--- app/index.tsx
+++ app/index.tsx
@@ -5,19 +5,18 @@
 //import Latihan4 from "@/components/latihan/Latihan4";
 import HomeScreen from "@/components/tugas/HomeScreen";
 import React, { useState } from "react";
-import { View } from "react-native";
 
 export default function App() {
   const [nama, setNama] = useState("");
 
   return (
-    <View>
+    <>
       {/*<Latihan1/>*/}
       {/*<LoginScreen />*/}
       {/*<Latihan2/>*/}
       {/*<Latihan3 />*/}
       {/*<Latihan4/>*/}
       <HomeScreen/>
-    </View>
+    </>
   );
 }
--- components/tugas/HomeScreen.tsx
+++ components/tugas/HomeScreen.tsx
@@ -1,9 +1,42 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
     return(
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+            <Image 
+            style={{ width: 32, height: 32}}
+            source={require("@/assets/images/react-logo.png")}
+            />
+            <Text style={styles.kodeinText}>Kodein</Text>
+            <Text style={styles.notesText}>Notes</Text>
+            </View>
+
+            <View style={styles.content}>
+                <Text>Content</Text>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: "white",
+    },
+    header: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    kodeinText: {
+        fontSize: 20,
+        fontWeight: 700,
+    },
+    notesText: {
+        fontSize: 20,
+        fontWeight: 700,
+        color: "#FF5B13"
+    },
+    content: {},
+})
